60eec2845959f4e854dc5f853500221d92d5f0da
Siddharth Maddheshiya <bhaisiddharth63@gmail.com>
2024-08-23 06:42:51 -0700

    Expo website

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Landing from "./Components/Landing";
+import Navbar from "./Components/Navbar";
+
+export default function App() {
+  return (
+    <div className="w-screen h-screen bg-zinc-900">
+      <Navbar />
+      <Landing/>
+    </div>
+  );
+}
